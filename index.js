da471ee882f4804985a43ab565f1af4a338cabc7
Kevin <klums24@gmail.com>
2023-04-26 10:46:18 -0700

    manually merged group's code in fear of permanent deletion :)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //! GLOBAL VARIABLES
-
 const nav = document.querySelector("#movie-nav") // this is where the movie navs go
 const seasonList = document.querySelector("#season-list") // name of all seasons 1-6
 const spotlightSeason = document.querySelector("#spotlight-season") // spotlight spot for season names
@@ -19,12 +18,10 @@ const spotlightEpisode = (episodeObj) => {
 spotlightImg.src = episodeObj[0].image
 episodeObj.forEach(episode => {
     let episodeList = document.createElement("li")
-    episodeList.innerText = episode.name 
-    spotlightEpisodeList.append(episodeList)  
+    episodeList.innerText = episode.name
+    spotlightEpisodeList.append(episodeList)       
 })
 }
-
-
 
 
 //Function renders each seasons title and image and appends it to nav
@@ -43,62 +40,6 @@ const handleNavClick = (seasonClick) => {
     spotlightEpisodeList.innerHTML = ""
     spotlightEpisode(seasonClick);
 }
-
-
-
-
-// Functions GETS Season 1 Data
-function fetchSeason1(url) {
-    fetch(url)
-    .then (res => res.json())
-    .then (episodes => { 
-        renderSeason(episodes)
-        spotlightEpisode(episodes);
-        handleNavClick(episodes)
-    })};
-fetchSeason1("http://localhost:3000/season1");
-
-// Functions GETS Season 2 Data
-function fetchSeason2(url) {
-    fetch(url)
-    .then (res => res.json())
-    .then (episodes => { 
-        renderSeason(episodes)
-        handleNavClick(episodes)
-    })};
-fetchSeason2("http://localhost:3000/season2");
-
-// Functions GETS Season 3 Data
-function fetchSeason3(url) {
-    fetch(url)
-    .then (res => res.json())
-    .then (episodes => { 
-        renderSeason(episodes)
-        // spotlightEpisode(episode);
-    })};
-fetchSeason3("http://localhost:3000/season3");
-
-// Functions GETS Season 4 Data
-function fetchSeason4(url) {
-    fetch(url)
-    .then (res => res.json())
-    .then (episodes => { 
-        renderSeason(episodes)
-        // spotlightEpisode(episode);
-    })};
-fetchSeason4("http://localhost:3000/season4");
-
-// Functions GETS Season 5 Data
-function fetchSeason5(url) {
-    fetch(url)
-    .then (res => res.json())
-    .then (episodes => { 
-        renderSeason(episodes)
-        // spotlightEpisode(episode);
-    })};
-fetchSeason5("http://localhost:3000/season5");
-
-
 
 //updateImages function to be used for renderCharForOption
 function updateImages(){
@@ -138,10 +79,25 @@ fetch ("http://localhost:3000/characters")
     charNames.forEach(charName =>renderCharForOption(charName))
     }   
 )
-    
-
-   
-    
-    
 
 
+//FETCH CALL
+const allSeasons = [
+    fetch("http://localhost:3000/season1"),
+    fetch("http://localhost:3000/season2"),
+    fetch("http://localhost:3000/season3"),
+    fetch("http://localhost:3000/season4"),
+    fetch("http://localhost:3000/season5")
+    ]
+    
+    Promise.all(allSeasons)
+    .then ((res) => {
+        Promise.all(res.map((season) => {
+            return season.json();
+        }))
+        .then(episodes => episodes.forEach(episode => {
+            spotlightEpisode(episode)
+            renderSeason(episode)
+            handleNavClick(episode)
+        }))
+    })
